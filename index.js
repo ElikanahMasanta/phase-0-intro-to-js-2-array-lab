511b5c58af731a29dd9540bad0fd3031f0c0ae6e
ElikanahMasanta <emasanta8@gmail.com>
2022-08-18 20:26:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-// Write your solution here!
-const kittens = ['Milo', 'Otis', 'Garfield'];
+// Write your solution here!var cats = ["Milo", "Garfield", "Otis"]
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+   }
+
+   function destructivelyprependCat(name){
+      cats.prepend(name)
+    return cats
+   }
+    function destructivelyRemoveLastCat(name){
+        cats.pop(name)
+        return cats
+    } 
+
+    function destructivelyRemoveFirstCat(name){
+        cats.slice(-2)
+        return cats
+    }
